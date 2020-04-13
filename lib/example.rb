5953178ab55e813e4e163a70f23d4b7deb115631
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-13 20:09:25 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,15 @@
 # Use this space to code along with the readme
 
 def say_hello_world_ten_times
-  puts "Hello World!"
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end 
